eaa0df9210114630d6ef20a5327bfe677777e4b9
aunshpatel <patelaunsh28@gmail.com>
2025-03-15 00:28:26 -0400

    Add filter to sort the final price and carat weight

--- lib/blocs/diamond_bloc.dart
+++ lib/blocs/diamond_bloc.dart
@@ -15,30 +15,55 @@ class FilterDiamonds extends DiamondEvent {
   FilterDiamonds({this.minCarat, this.maxCarat, this.shape, this.lab, this.color, this.clarity});
 }
 
+class SortDiamonds extends DiamondEvent {
+  final String sortBy;
+  final bool? ascending;
+
+  SortDiamonds({required this.sortBy, this.ascending});
+}
+
 abstract class DiamondState {}
 
 class DiamondLoading extends DiamondState {}
 
 class DiamondLoaded extends DiamondState {
   final List<Diamond> diamonds;
-  DiamondLoaded(this.diamonds);
+  final List<Diamond> originalDiamonds;
+
+  DiamondLoaded(this.diamonds, this.originalDiamonds);
 }
 
 class DiamondBloc extends Bloc<DiamondEvent, DiamondState> {
-  DiamondBloc() : super(DiamondLoaded(diamondList)) {
+  DiamondBloc() : super(DiamondLoaded(diamondList, diamondList)) {
     on<FilterDiamonds>((event, emit) {
       emit(DiamondLoading());
 
       List<Diamond> filteredDiamonds = diamondList.where((d) {
         return (event.minCarat == null || d.carat >= event.minCarat!) &&
             (event.maxCarat == null || d.carat <= event.maxCarat!) &&
-            (event.shape == null || d.shape == event.shape) &&
-            (event.lab == null || d.lab == event.lab) &&
-            (event.color == null || d.color == event.color) &&
-            (event.clarity == null || d.clarity == event.clarity);
+            (event.shape == null || event.shape == "All" || d.shape == event.shape) &&
+            (event.lab == null || event.lab == "All" || d.lab == event.lab) &&
+            (event.color == null || event.color == "All" || d.color == event.color) &&
+            (event.clarity == null || event.clarity == "All" || d.clarity == event.clarity);
       }).toList();
 
-      emit(DiamondLoaded(filteredDiamonds));
+      emit(DiamondLoaded(filteredDiamonds, List.from(filteredDiamonds)));
+    });
+
+    on<SortDiamonds>((event, emit) {
+      if (state is DiamondLoaded) {
+        List<Diamond> sortedDiamonds = List.from((state as DiamondLoaded).originalDiamonds);
+
+        if (event.ascending != null) {
+          if (event.sortBy == "price") {
+            sortedDiamonds.sort((a, b) => event.ascending! ? a.finalAmount.compareTo(b.finalAmount) : b.finalAmount.compareTo(a.finalAmount));
+          } else if (event.sortBy == "carat") {
+            sortedDiamonds.sort((a, b) => event.ascending! ? a.carat.compareTo(b.carat) : b.carat.compareTo(a.carat));
+          }
+        }
+
+        emit(DiamondLoaded(sortedDiamonds, (state as DiamondLoaded).originalDiamonds));
+      }
     });
   }
 }
--- lib/screens/result_page.dart
+++ lib/screens/result_page.dart
@@ -13,96 +13,156 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
-  @override
+  String _selectedPriceOrder = "Default";
+  String _selectedCaratOrder = "Default";
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Diamond Results')),
-      body: BlocBuilder<DiamondBloc, DiamondState>(
-        builder: (context, state) {
-          if (state is DiamondLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is DiamondLoaded) {
-            final List<Diamond> diamonds = state.diamonds;
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Final Price Filter:', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    buildSortDropdown(_selectedPriceOrder, ["Default", "Low to High", "High to Low"],
+                      (value) {
+                        setState(() {
+                          _selectedPriceOrder = value;
+                        });
 
-            if (diamonds.isEmpty) {
-              return Center(child: Text('No diamonds match the selected criteria.'));
-            }
-
-            return ListView.builder(
-              itemCount: diamonds.length,
-              itemBuilder: (context, index) {
-                final diamond = diamonds[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  elevation: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Flexible(
-                              child: Text("${index + 1}) Lot ID: ${diamond.lotID}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 8.0),
-                              child: Material(
-                                color:Color(0XFF3A4355),
-                                elevation: 5.0,
-                                borderRadius: BorderRadius.circular(30.0),
-                                child: MaterialButton(
-                                  onPressed: () {
-                                    context.read<CartBloc>().add(AddToCart(diamond));
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(content: Text("${diamond.lotID} added to cart"))
-                                    );
-                                  },
-                                  minWidth: 50.0,
-                                  // height: 20.0,
-                                  child: Text(
-                                    'Add to Cart',
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white
-                                    )
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Divider(),
-                        Text("Carat: ${diamond.carat} ct", style: TextStyle(fontSize: 16)),
-                        Text("Size: ${diamond.size}", style: TextStyle(fontSize: 16)),
-                        Text("Shape: ${diamond.shape}", style: TextStyle(fontSize: 16)),
-                        Text("Lab: ${diamond.lab}", style: TextStyle(fontSize: 16)),
-                        Text("Color: ${diamond.color}", style: TextStyle(fontSize: 16)),
-                        Text("Clarity: ${diamond.clarity}", style: TextStyle(fontSize: 16)),
-                        Text("Cut: ${diamond.cut}", style: TextStyle(fontSize: 16)),
-                        Text("Polish: ${diamond.polish}", style: TextStyle(fontSize: 16)),
-                        Text("Symmetry: ${diamond.symmetry}", style: TextStyle(fontSize: 16)),
-                        Text("Fluorescence: ${diamond.fluorescence}", style: TextStyle(fontSize: 16)),
-                        Text("Discount: ${diamond.discount}%", style: TextStyle(fontSize: 16, color: Colors.red)),
-                        Text("Per Carat Rate: \$${diamond.perCaratRate.toStringAsFixed(2)}", style: TextStyle(fontSize: 16)),
-                        Text("Final Amount: \$${diamond.finalAmount.toStringAsFixed(2)}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green)),
-                        SizedBox(height: 5),
-                        Text("Key To Symbol: ${diamond.keyToSymbol}", style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
-                        Text("Lab Comment: ${diamond.labComment}", style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
-                      ],
+                        context.read<DiamondBloc>().add(
+                          SortDiamonds(
+                            sortBy: "price",
+                            ascending: value == "Low to High" ? true : value == "High to Low" ? false : null,
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                );
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Carat Weight Filter:', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    buildSortDropdown(_selectedCaratOrder, ["Default", "Low to High", "High to Low"],
+                      (value) {
+                        setState(() {
+                          _selectedCaratOrder = value;
+                        });
+
+                        context.read<DiamondBloc>().add(
+                          SortDiamonds(
+                            sortBy: "carat",
+                            ascending: value == "Low to High" ? true : value == "High to Low" ? false : null,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: BlocBuilder<DiamondBloc, DiamondState>(
+              builder: (context, state) {
+                if (state is DiamondLoading) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is DiamondLoaded) {
+                  final List<Diamond> diamonds = state.diamonds;
+
+                  if (diamonds.isEmpty) {
+                    return Center(child: Text('No diamonds match the selected criteria.'));
+                  }
+
+                  return ListView.builder(
+                    itemCount: diamonds.length,
+                    itemBuilder: (context, index) {
+                      final diamond = diamonds[index];
+                      return Card(
+                        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        elevation: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      "${index + 1}) Lot ID: ${diamond.lotID}",
+                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                    child: Material(
+                                      color: Color(0XFF3A4355),
+                                      elevation: 5.0,
+                                      borderRadius: BorderRadius.circular(30.0),
+                                      child: MaterialButton(
+                                        onPressed: () {
+                                          context.read<CartBloc>().add(AddToCart(diamond));
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            SnackBar(content: Text("${diamond.lotID} added to cart")),
+                                          );
+                                        },
+                                        minWidth: 50.0,
+                                        child: Text(
+                                          'Add to Cart',
+                                          style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Divider(),
+                              Text("Carat: ${diamond.carat} ct", style: TextStyle(fontSize: 16)),
+                              Text("Size: ${diamond.size}", style: TextStyle(fontSize: 16)),
+                              Text("Shape: ${diamond.shape}", style: TextStyle(fontSize: 16)),
+                              Text("Lab: ${diamond.lab}", style: TextStyle(fontSize: 16)),
+                              Text("Color: ${diamond.color}", style: TextStyle(fontSize: 16)),
+                              Text("Clarity: ${diamond.clarity}", style: TextStyle(fontSize: 16)),
+                              Text("Cut: ${diamond.cut}", style: TextStyle(fontSize: 16)),
+                              Text("Polish: ${diamond.polish}", style: TextStyle(fontSize: 16)),
+                              Text("Symmetry: ${diamond.symmetry}", style: TextStyle(fontSize: 16)),
+                              Text("Fluorescence: ${diamond.fluorescence}", style: TextStyle(fontSize: 16)),
+                              Text("Discount: ${diamond.discount}%", style: TextStyle(fontSize: 16, color: Colors.red)),
+                              Text("Per Carat Rate: \$${diamond.perCaratRate.toStringAsFixed(2)}", style: TextStyle(fontSize: 16)),
+                              Text(
+                                "Final Amount: \$${diamond.finalAmount.toStringAsFixed(2)}",
+                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green),
+                              ),
+                              SizedBox(height: 5),
+                              Text("Key To Symbol: ${diamond.keyToSymbol}", style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
+                              Text("Lab Comment: ${diamond.labComment}", style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return Center(child: Text('Unexpected state. Please try again.'));
+                }
               },
-            );
-          } else {
-            return Center(child: Text('Unexpected state. Please try again.'));
-          }
-        },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -112,7 +172,24 @@ class _ResultPageState extends State<ResultPage> {
           );
         },
         backgroundColor: Color(0XFF3A4355),
-        child: const Icon(Icons.shopping_cart, color: Colors.white,),
+        child: const Icon(Icons.shopping_cart, color: Colors.white),
+      ),
+    );
+  }
+
+  /// Dropdown Builder Function
+  Widget buildSortDropdown(String selectedValue, List<String> options, Function(String) onChanged) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: DropdownButton<String>(
+        value: selectedValue,
+        onChanged: (value) => onChanged(value!),
+        items: options.map((label) {
+          return DropdownMenuItem(
+            value: label,
+            child: Text(label),
+          );
+        }).toList(),
       ),
     );
   }
